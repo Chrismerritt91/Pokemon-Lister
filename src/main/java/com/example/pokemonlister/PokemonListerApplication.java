e75965ac9d7076a9dc775e00fd737b92597f94d0
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-09-06 16:01:14 -0500

    first commit

---
+++ src/main/java/com/example/pokemonlister/PokemonListerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokemonlister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonListerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonListerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pokemonlister/PokemonListerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonlister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonListerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
