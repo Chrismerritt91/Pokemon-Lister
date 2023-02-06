9821303da833c4e6229607d84c089ba1aaba05f0
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-02-06 17:56:56 -0600

    run fetch request and look over data

---
+++ src/main/resources/static/js/keys.js
@@ -0,0 +1,1 @@
+export const POKE_APP_API = "https://pokeapi.co/api/v2/"
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,16 @@
+import {POKE_APP_API} from "./keys.js";
 
+let pokemon = "pokemon/?limit=1279"
 
+export const runPokeApp = () => {
+    fetch(POKE_APP_API + pokemon)
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+    }
+runPokeApp()
 
-// export const runPokeApp = () => {
-//     fetch(POKE_APP_API)
-//         .then((res) => res.json())
-//         .then($("#loader").addClass("hidden"))
-//         .then((res) => {
-//             $("#content").html("")
-// }
+// .then($("#loader").addClass("hidden"))
+        // .then((res) => {
+        //     $("#content").html("")
+        //     console.log(res);
+        // })};
