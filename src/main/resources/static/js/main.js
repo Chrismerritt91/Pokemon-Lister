49efe9a93bf74437ce6db96a063dedc2da20f20a
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-02-06 17:22:59 -0600

    make changes to file structure and begin layout of index.html

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,10 @@
+
+
+
+// export const runPokeApp = () => {
+//     fetch(POKE_APP_API)
+//         .then((res) => res.json())
+//         .then($("#loader").addClass("hidden"))
+//         .then((res) => {
+//             $("#content").html("")
+// }
