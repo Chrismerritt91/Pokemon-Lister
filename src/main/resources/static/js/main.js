1e540edb633341668fc803ca607c6031e3035118
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-02-07 13:56:08 -0600

    fetch data and troubleshoot promises, work on layout for individual pokemon

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,16 +1,41 @@
 import {POKE_APP_API} from "./keys.js";
 
-let pokemon = "pokemon/?limit=1279"
+// /?limit=1279 append this to the end of query for all pokemon, too much for page initial limit is ?offset=20&limit=20
+let pokeQuery = "pokemon"
+
+const createPokemonCard = ({name, id, sprites}) => {
+    document.getElementById("content").innerHTML +=
+        `
+        <div data-id="${id}" id="card" class="border border-warning border-4 rounded bg-secondary text-center card">
+        <img class="card-img-top" src="${sprites.front_default}" alt="Poke image">
+        <h4 class="name">${name}</h4>
+        <h6 class="pokeNum">NO. ${id}</h6>
+        </div>
+        `
+}
 
 export const runPokeApp = () => {
-    fetch(POKE_APP_API + pokemon)
+    fetch(POKE_APP_API + pokeQuery)
         .then((res) => res.json())
-        .then((res) => console.log(res))
-    }
+        // .then((res) => console.log(res))
+        .then($("#loader").addClass("hidden"))
+        .then((res) => {
+            $("#content").html("")
+            console.log(res)
+            res.results.forEach((pokemon) => {
+                if(typeof pokemon.name === 'string' && pokemon.name !== ''){
+                    getPokemon(pokemon.name).then((res) => {
+                        createPokemonCard(res)
+                    })
+                }
+            })
+        })
+}
+
+const getPokemon = name => {
+    return fetch(POKE_APP_API + pokeQuery + "/" + name, {method: "GET"})
+        .then((res) => res.json())
+}
+
 runPokeApp()
 
-// .then($("#loader").addClass("hidden"))
-        // .then((res) => {
-        //     $("#content").html("")
-        //     console.log(res);
-        // })};
